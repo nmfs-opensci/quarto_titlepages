b40413faaa086bf1fe064af8a87322f075abcbd7
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-08-27 20:51:46 +0200

    fix: path issue in templates

--- _extensions/titlepage/theme.lua
+++ _extensions/titlepage/theme.lua
@@ -11,7 +11,7 @@ function Meta(m)
     ["bg-image"] = function (m)
       m['bg-image'] = true
       if isEmpty(m['title-bg-image']) then
-        m['title-bg-image'] = "bg-images/images/corner-bg.png"
+        m['title-bg-image'] = "corner-bg.png"
       end
       if isEmpty(m['title-bg-location']) then
         m['title-bg-location'] = "UL"
@@ -19,25 +19,25 @@ function Meta(m)
       if isEmpty(m['title-bg-size']) then
         m['title-bg-size'] = 0.5
       end
-      if isEmpty(m['title-geometry']) then
-        m['titlepage-geometry'] = {top = "3in", bottom = "1in", right = "1in", left = "1in"}
-      end
+      -- if isEmpty(m['title-geometry']) then
+      --   m['titlepage-geometry'] = {top = "3in", bottom = "1in", right = "1in", left = "1in"}
+      -- end
       return m
     end,
     ["classic-lined-static"] = function (m)
       m['classic-lined-static'] = true
       if isEmpty(m['logo']) then
-        m['logo'] = "classic-lined-static/logo.png"
+        m['logo'] = "logo.png"
       end
       return m
     end,
     ["great-wave"] = function (m)
       m['great-wave'] = true
-      if isEmpty(m['title-geometry']) then
-        m['titlepage-geometry'] = {top = "3in", bottom = "0in", right = "1in", left = "1in"}
-      end
+      -- if isEmpty(m['title-geometry']) then
+      --   m['titlepage-geometry'] = {top = "3in", bottom = "0in", right = "1in", left = "1in"}
+      -- end
       if isEmpty(m['title-bg-image']) then
-        m['title-bg-image'] = "great-wave/images/TheGreatWaveoffKanagawa.jpeg"
+        m['title-bg-image'] = "TheGreatWaveoffKanagawa.jpeg"
       end
       if isEmpty(m['title-page-color']) then
         m['title-page-color'] = "F6D5A8"
@@ -47,28 +47,28 @@ function Meta(m)
     ["multi-purpose-static"] = function (m)
       m['multi-purpose-static'] = true
       if isEmpty(m['logo']) then
-        m['logo'] = "multi-purpose-static/logo.png"
+        m['logo'] = "logo.png"
       end
       return m
     end,
     ["thesis-static"] = function (m)
       m['thesis-static'] = true
       if isEmpty(m['logo']) then
-        m['logo'] = "thesis-static/logo.png"
+        m['logo'] = "logo.png"
       end
       return m
     end,
     ["vline"] = function (m)
       m['vline'] = true
       if isEmpty(m['logo']) then
-        m['logo'] = "vline/logo.png"
+        m['logo'] = "logo.png"
       end
       return m
     end,
     ["vline-static"] = function (m)
       m['vline-static'] = true
       if isEmpty(m['logo']) then
-        m['logo'] = "vline-static/logo.png"
+        m['logo'] = "logo.png"
       end
       return m
     end
