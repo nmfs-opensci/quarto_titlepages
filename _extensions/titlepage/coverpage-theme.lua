077a486381a9b9426f224b5b26b4b119b5697c58
Eli Holmes <eli.holmes@noaa.gov>
2022-09-29 14:46:32 -0700

    ad nmfs-opensci to the path

--- _extensions/titlepage/coverpage-theme.lua
+++ _extensions/titlepage/coverpage-theme.lua
@@ -219,7 +219,7 @@ Set up the demos
   choice = pandoc.utils.stringify(m.coverpage)
   if choice == "great-wave" then
     if isEmpty(m['coverpage-bg-image']) then
-      m['coverpage-bg-image'] = "_extensions/titlepage/images/TheGreatWaveoffKanagawa.jpeg"
+      m['coverpage-bg-image'] = "_extensions/nmfs-opensci/titlepage/images/TheGreatWaveoffKanagawa.jpeg"
     end
     if isEmpty(m['coverpage-title']) then
       m['coverpage-title'] = "quarto_titlepages"
@@ -236,7 +236,7 @@ Set up the demos
   end
   if choice == "otter" then
     if isEmpty(m['coverpage-bg-image']) then
-      m['coverpage-bg-image'] = "_extensions/titlepage/images/otter-bar.jpeg"
+      m['coverpage-bg-image'] = "_extensions/nmfs-opensci/titlepage/images/otter-bar.jpeg"
     end
     if isEmpty(m['coverpage-title']) then
       m['coverpage-title'] = "Otters"
